b4717684c88f65e4aa3c6d8200ba9ca926dfb580
Amy Li <liamy1996@hotmail.com>
2019-03-15 04:21:30 -0400

    Updates

---
+++ precache-manifest.9233e9171eea0fcd067149214b4a1a39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/uwcsa/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cdded50e8234da0f8490",
+    "url": "/uwcsa/static/js/runtime~main.bd3ec780.js"
+  },
+  {
+    "revision": "598bb61ac0c3a9571e1e",
+    "url": "/uwcsa/static/js/main.3df55e34.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/uwcsa/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "598bb61ac0c3a9571e1e",
+    "url": "/uwcsa/static/css/main.c154d3d9.chunk.css"
+  },
+  {
+    "revision": "1845eb55aa6363518cc72f1a34d46751",
+    "url": "/uwcsa/index.html"
+  }
+];
